58da87bd14ce8cab65aa6b1fb2ff89a0d184461e
zafirfhn <zafirrrrr666@gmail.com>
2023-10-23 23:24:11 +0700

    mengedit banyak

--- learnJs1.js
+++ learnJs1.js
@@ -2,6 +2,12 @@ var ulang = false;
 while(ulang === false) {
 
     var nama = prompt("Masukkan Nama")
-    alert("fakyux " + nama)
-    ulang = confirm('pencet "OK" kalo setuju gwehj gamteng')
+    alert("fakyu " + nama)
+    ulang = confirm('pencet "OK" kalo setuju gwehj ganteng keren dan asikk');
+    
+}
+
+if(ulang === true) {
+    alert('Makasih, iya gua tau kalo gua seganteng sekeren dan seasik itu')
+
 }
